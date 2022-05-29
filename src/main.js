d29c51d0e8d43a2a0c59e7230a690ef2f39c2a3d
Marifatin Azizah <amarifatin@gmail.com>
2022-05-29 21:08:36 +0700

    fix crud vessel

--- src/main.js
+++ src/main.js
@@ -14,16 +14,21 @@ import emitter from 'mitt'
 import VueSweetalert2 from 'vue-sweetalert2'
 import 'sweetalert2/dist/sweetalert2.min.css'
 
+import vSelect from 'vue-select'
+import 'vue-select/dist/vue-select.css'
+
 const app = createApp(App)
 app.config.globalProperties.emitter = emitter()
 app.use(store)
 app.use(router)
 app.use(CoreuiVue)
 app.use(VueSweetalert2)
+app.use()
 app.provide('icons', icons)
 app.component('CIcon', CIcon)
 app.component('DocsCallout', DocsCallout)
 app.component('DocsExample', DocsExample)
 app.component('data-index', DataIndex)
+app.component('v-select', vSelect)
 
 app.mount('#app')
--- src/views/vessel/Index.vue
+++ src/views/vessel/Index.vue
@@ -1,21 +1,96 @@
 <template>
   <div>
     <data-index
-      title="Vessels Type Data"
+      title="Vessel Data"
       :columns="columns"
       :actions="actions"
-      @test-emit="testEmit"
-    />
+      @data-edit="handleEdit"
+      @data-delete="handleDelete"
+    >
+      <template #top>
+        <button class="btn btn-primary mb-3" @click="() => handleCreate()">
+          Create New Vessel
+        </button>
+      </template>
+    </data-index>
     <c-modal
-      size="sm"
+      alignment="center"
       :visible="toggleForm"
       @close="() => (toggleForm = !toggleForm)"
     >
+      <c-modal-header>
+        <c-modal-title>
+          {{ isEdit ? 'Edit' : 'Create New' }} Vessel
+        </c-modal-title>
+      </c-modal-header>
+      <c-modal-body>
+        <div class="form-group">
+          <label for="">Owner</label>
+          <v-select
+            label="name"
+            :options="owner"
+            v-model="ownerSelected"
+            :value="ownerSelected"
+          />
+        </div>
+        <div class="form-group">
+          <label>Name</label>
+          <input type="text" class="form-control" v-model="name" />
+        </div>
+        <div class="form-group">
+          <label for="">Vessel Type</label>
+          <v-select
+            label="name"
+            :options="type"
+            v-model="typeSelected"
+            :value="typeSelected"
+          />
+        </div>
+        <div class="form-group">
+          <label>IMO</label>
+          <input type="text" class="form-control" v-model="imo" />
+        </div>
+        <div class="form-group">
+          <label>Call Signin</label>
+          <input type="text" class="form-control" v-model="callSignin" />
+        </div>
+        <div class="form-group">
+          <label>Length</label>
+          <input type="text" class="form-control" v-model="length" />
+        </div>
+        <div class="form-group">
+          <label>Width</label>
+          <input type="text" class="form-control" v-model="width" />
+        </div>
+        <div class="form-group">
+          <label>Depth</label>
+          <input type="text" class="form-control" v-model="depth" />
+        </div>
+        <div class="form-group">
+          <label>GT (Gross tonase)</label>
+          <input type="text" class="form-control" v-model="gt" />
+        </div>
+        <div class="form-group">
+          <label>Netto</label>
+          <input type="text" class="form-control" v-model="netto" />
+        </div>
+        <div class="form-group">
+          <label>Year</label>
+          <input type="text" class="form-control" v-model="year" />
+        </div>
+      </c-modal-body>
+      <c-modal-footer>
+        <c-button color="primary" @click="isEdit ? dataEdited() : dataCreated()"
+          >Save</c-button
+        >
+      </c-modal-footer>
     </c-modal>
   </div>
 </template>
 
 <script>
+import dataServices from '@/services/dataServices'
+
 const columns = [
   {
     name: 'Name',
@@ -38,23 +113,23 @@ const columns = [
     dataIndex: 'call_signin',
   },
   {
-    name: 'Length',
+    name: 'Length (m)',
     dataIndex: 'length',
   },
   {
-    name: 'Width',
+    name: 'Width (m)',
     dataIndex: 'width',
   },
   {
-    name: 'Depth',
+    name: 'Depth (m)',
     dataIndex: 'depth',
   },
   {
-    name: 'GT',
+    name: 'GT (ton)',
     dataIndex: 'gt',
   },
   {
-    name: 'Netto',
+    name: 'Netto (ton)',
     dataIndex: 'netto',
   },
   {
@@ -66,24 +141,158 @@ const columns = [
 const actions = [
   {
     title: 'Edit',
-    emit: 'test-emit',
+    emit: 'data-edit',
     class: 'btn-primary',
+  },
+  {
+    title: 'Delete',
+    emit: 'data-delete',
+    class: 'btn-outline-danger',
   },
 ]
 export default {
   data() {
     return {
+      apiPath: '/vessels',
       columns,
       actions,
-      apiPath: '/vessels',
+      toggleForm: false,
+      isEdit: false,
+      type: [],
+      owner: [],
+      id: '',
+      typeSelected: {},
+      ownerSelected: {},
+      name: '',
+      imo: '',
+      callSignin: '',
+      length: '',
+      width: '',
+      depth: '',
+      gt: '',
+      netto: '',
+      year: '',
     }
   },
   mounted() {
     this.emitter.emit('fetch', this.apiPath)
+    this.initData()
   },
   methods: {
-    testEmit(data) {
-      console.log(data)
+    async initData() {
+      try {
+        const type = await dataServices.dataIndex('/vessel-type')
+        this.type = type.data.result
+
+        const owner = await dataServices.dataIndex('/fisherman')
+        this.owner = owner.data.result
+      } catch (err) {
+        console.log(err)
+      }
+    },
+    handleCreate() {
+      this.resetData()
+      this.toggleForm = true
+    },
+    async dataCreated() {
+      const data = {
+        name: this.name,
+        fishermanId: this.ownerSelected.id,
+        vesselTypeId: this.typeSelected.id,
+        imo: this.imo,
+        callSignin: this.callSignin,
+        length: this.length,
+        width: this.width,
+        depth: this.depth,
+        gt: this.gt,
+        netto: this.netto,
+        year: this.year,
+      }
+
+      try {
+        this.toggleForm = false
+        await this.$store.dispatch('data/create', { path: this.apiPath, data })
+        this.$store.dispatch('data/index', { path: this.apiPath })
+      } catch (err) {
+        console.log(err)
+      }
+    },
+    handleEdit(data) {
+      this.resetData()
+      this.name = data.name
+      this.ownerSelected =
+        this.owner.find((f) => f.id == data.fisherman_id) ?? {}
+      this.typeSelected =
+        this.type.find((f) => f.id == data.vessels_type_id) ?? {}
+      this.imo = data.imo
+      this.callSignin = data.call_signin
+      this.length = data.length
+      this.width = data.width
+      this.depth = data.depth
+      this.gt = data.gt
+      this.netto = data.netto
+      this.year = data.year
+      this.id = data.id
+
+      this.isEdit = true
+      this.toggleForm = true
+    },
+    async dataEdited() {
+      const data = {
+        name: this.name,
+        fishermanId: this.ownerSelected.id,
+        vesselTypeId: this.typeSelected.id,
+        imo: this.imo,
+        callSignin: this.callSignin,
+        length: this.length,
+        width: this.width,
+        depth: this.depth,
+        gt: this.gt,
+        netto: this.netto,
+        year: this.year,
+      }
+
+      await this.$store.dispatch('data/update', {
+        path: this.apiPath,
+        id: this.id,
+        data,
+      })
+
+      this.$store.dispatch('data/index', { path: this.apiPath })
+    },
+    handleDelete(data) {
+      this.name = data.name
+      this.id = data.id
+      this.$swal({
+        icon: 'question',
+        title: 'Are you sure?',
+        html: `Want to delete <strong> ${this.name} </strong>`,
+        confirmButtonText: 'Yes, delete it!',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.dataDeleted()
+        }
+      })
+    },
+    async dataDeleted() {
+      await this.$store.dispatch('data/delete', {
+        path: this.apiPath,
+        id: this.id,
+      })
+      this.$store.dispatch('data/index', { path: this.apiPath })
+    },
+    resetData() {
+      this.typeSelected = {}
+      this.ownerSelected = {}
+      this.name = ''
+      this.imo = ''
+      this.callSignin = ''
+      this.length = ''
+      this.width = ''
+      this.depth = ''
+      this.gt = ''
+      this.netto = ''
+      this.year = ''
     },
   },
 }
